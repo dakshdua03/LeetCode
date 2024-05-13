faa4886cd7f9e75ce9f61d73e579ecb749ce388f
dakshdua03 <dakshdua03@gmail.com>
2024-05-13 21:07:23 +0530

    day 4

---
+++ scoreafterflippingmatrix.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int matrixScore(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        for(int i = 0; i < m; i++)
+        {
+            if(grid[i][0] == 0)
+            {
+                //flip
+
+                for(int j = 0; j < n; j++)
+                {
+                    grid[i][j] = 1-grid[i][j];
+                }
+            }
+        }
+
+        for(int j = 1; j < n; j++)
+        {
+            int countZero = 0;
+            for(int i = 0; i < m; i++)
+            {
+                if(grid[i][j] == 0)
+                {
+                    countZero++;
+                }
+            }
+            int countOne = m - countZero;
+            if(countZero > countOne)
+            {
+                //flipping 
+                for(int i = 0; i < m; i++)
+                {
+                    grid[i][j] = 1-grid[i][j];
+                }
+            }
+        }
+         int score = 0;
+            for(int i = 0; i < m; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    int value = grid[i][j]*pow(2, n-j-1);
+
+                    score += value;
+                }
+            }
+
+         return score;
+    }
+};
